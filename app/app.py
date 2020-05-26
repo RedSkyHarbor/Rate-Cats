c33b365e63de764b9488921daf6394fc5fd9e82d
Jesse Breuer-Penello <PenelloJ2@gmail.com>
2020-05-26 16:02:46 -0400

    request img from cat api and display it

--- app/app.py
+++ app/app.py
@@ -1,7 +1,14 @@
 from flask import Flask, render_template
+import os
+import requests
 
 app = Flask(__name__)
 
+API_KEY = os.environ.get('CAT_API_KEY', None)
+CAT_SEARCH_URL = 'https://api.thecatapi.com/v1/images/search'
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    r = requests.get(CAT_SEARCH_URL, headers={'x-api-key': API_KEY})
+    image = r.json()[0]
+    return render_template('index.html', cat=image)
